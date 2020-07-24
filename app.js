e3cf38327cc4837f615c04f51cdc083d8d4c182d
Ethan Torres <torresethan99@gmail.com>
2020-07-23 20:50:09 -0500

    portfolio generator version 1.1.7

---
+++ app.js
@@ -0,0 +1,24 @@
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+const printProfileData = (profileDataArr) => {
+  for (let i = 0; i < profileDataArr.length; i += 1){
+  //console.log(profileDataArr[i]);
+  }
+  //console.log('====================');
+  profileDataArr.forEach(profileItem => console.log(profileItem));
+};
+//printProfileData(profileDataArgs);
+
+const [name, github] = profileDataArgs;
+
+
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+  if (err) throw err;
+
+  console.log('portfolio complete! check out index.html')
+});
+
+
